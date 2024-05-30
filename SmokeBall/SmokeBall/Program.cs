634827209f2241ad2af255c0d683286151f0e4b2
Vinh D Tran <vinhdt90@outlook.com>
2024-05-31 08:15:52 +1000

    Add prototype

---
+++ SmokeBall/SmokeBall/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+var content = File.ReadAllText("searchResult.txt");
+
+// Website url are contained in div's with class MjjYud
+string pattern = "<div\\s+class=\"MjjYud\"[^>]*>(.*?)</div>";
+Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+
+var matches = regex.Matches(content);
+
+StringBuilder sb = new StringBuilder();
+for (int i = 0; i < matches.Count; i++)
+{
+    if (matches[i].Value.Contains("www.smokeball.com.au"))
+    {
+        sb.Append(i + 1).Append(", ");
+    }
+}
+
+// Remove comma from end of line. 
+if (sb.Length > 0)
+{
+    sb.Remove(sb.Length - 2, 1);
+}
+
+Console.WriteLine(sb.ToString()); ;
---
+++ SmokeBall/SmokeBall/WebScraper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SmokeBall
+{
+    internal class WebScraper
+    {
+
+        public static async Task ScrapeAsync(string url)
+        {
+            HttpClient client = new HttpClient();
+
+            // Set user-agent to avoid being blocked by Google
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
+
+            string htmlContent = await client.GetStringAsync(url);
+
+            // Simple parsing using regex (this example extracts titles of search results)
+            Regex regex = new Regex("<h3 class=\".*?\">(.*?)</h3>", RegexOptions.Singleline);
+            MatchCollection matches = regex.Matches(htmlContent);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Groups[1].Value);
+            }
+
+        }
+    }
+}

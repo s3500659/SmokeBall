b72f5779d4c112f2b1f730bdc96d7b2c922ceb71
Vinh D Tran <vinhdt90@outlook.com>
2024-05-31 20:06:48 +1000

    Add controller project

---
+++ SmokeBall/API/Controllers/SearchController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SearchController : ControllerBase
+    {
+        public SearchController()
+        {
+
+        }
+
+
+        [HttpGet(Name = "GetSearchResult")]
+        public string Get()
+        {
+            return "Your results!";
+        }
+    }
+}
--- SmokeBall/WPF/SearchEngineService.cs
+++ SmokeBall/WPF/SearchEngineService.cs
@@ -1,6 +1,12 @@
-﻿namespace WPF
+﻿using System;
+
+namespace WPF
 {
     internal class SearchEngineService
     {
+        internal static string Search(string keywords, string searchLimit, SearchEngine google)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
